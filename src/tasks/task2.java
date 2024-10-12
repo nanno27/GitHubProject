fd04f52c4a85b0091ab3d4d3b87b0d6b686e86c6
nanno27 <nik.nanovich@gmail.com>
2024-10-13 02:58:41 +0300

    Второе задание

---
+++ src/tasks/task2.java
@@ -0,0 +1,43 @@
+package tasks;
+import java.util.Scanner;
+import java.util.Random;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int N = scanner.nextInt();
+
+        int [][] array = makeArray(N);
+        sumArrayColumns(array, N);
+    }
+
+    static int[][] makeArray(int N) {
+        Random random = new Random();
+        int rows = N;
+        int columns = N;
+        int[][] array = new int[rows][columns];
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                array[i][j] = random.nextInt(0, 10);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return array;
+    }
+
+    static void sumArrayColumns(int[][] array, int N) {
+        int rows = N;
+        int columns = N;
+        int sum = 0;
+        for (int i = 0; i < rows; i++) {
+            sum = 0;
+            for (int j = 0; j < columns; j++) {
+                sum += array[j][i];
+            }
+            System.out.println("Сумма столбца " + (i + 1) + ": " + sum);
+        }
+    }
+
+}
