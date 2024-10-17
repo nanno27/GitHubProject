1602928762e6e972160d480d12c2fe4636fe4d6b
nanno27 <nik.nanovich@gmail.com>
2024-10-17 23:04:03 +0300

    Добавлены исключения для задачи 1 и 2

--- src/tasks/task1/Task1.java
+++ src/tasks/task1/Task1.java
@@ -1,19 +1,29 @@
 package tasks.task1;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task1 {
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите строку: ");
-        String string = scanner.nextLine();
-        System.out.println("Количество повторов: ");
-        final int N = scanner.nextInt();
+            try {
+                System.out.println("Введите строку: ");
+                String string = scanner.nextLine();
+                System.out.println("Количество повторов: ");
+                final int N = scanner.nextInt();
+                if (N <= 0) {
+                    throw new IllegalArgumentException("Количество повторов должно быть больше 0");
+                }
+                CharSequence result = seqString(string, N);
+                System.out.println(result);
 
-        CharSequence result = seqString(string, N);
+            } catch (InputMismatchException e) {
+                System.out.println("Введено некорректное значение");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                scanner.close();
+            }
 
-        System.out.println(result);
-
-        scanner.close();
     }
 
     static StringBuilder seqString(CharSequence charString, int N) {
--- src/tasks/task2/Task2.java
+++ src/tasks/task2/Task2.java
@@ -1,15 +1,27 @@
 package tasks.task2;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Random;
 
 public class Task2 {
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размерность двумерного массива: ");
-        final int N = scanner.nextInt();
+        try {
+            System.out.println("Введите размерность двумерного массива: ");
+            final int N = scanner.nextInt();
+            if (N <= 0) {
+                throw new IllegalArgumentException("Размерность массива должна быть больше 0");
+            }
+            int[][] array = makeArray(N);
+            sumArrayColumns(array, N);
 
-        int [][] array = makeArray(N);
-        sumArrayColumns(array, N);
+        } catch (InputMismatchException e) {
+            System.out.println("Введено некорректное значение");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 
     static int[][] makeArray(int N) {
@@ -18,7 +30,7 @@ public class Task2 {
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                array[i][j] = random.nextInt(0, 10);
+                array[i][j] = random.nextInt(-100, 100);
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
